fbdbd70effbc4ddadde2c938747938eb8a47fa67
lastlink <theefunk@gmail.com>
2021-02-24 20:21:22 -0500

    reformat and 404

---
+++ layouts/error.vue
@@ -0,0 +1,40 @@
+<template>
+  <main class="main">
+    <template v-if="error.statusCode === 404">
+      <h1 class="title mb-2">Page Not Found</h1>
+      <h2 class="subtitle">404</h2>
+      <p class="mb-4">
+        Looks like you've followed a broken link2 or entered a URL that doesn't
+        exist on this site.
+      </p>
+    </template>
+    <template v-else>
+      <h1 class="title">An error occurred</h1>
+      <p class="mb-4">
+        Looks like you've followed a broken link2 or entered a URL that doesn't
+        exist on this site.
+      </p>
+    </template>
+    <nuxt-link to="/">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        viewBox="0 0 16 16"
+        class="inline fill-current"
+      >
+        <path
+          d="M11.9998836,4.09370803 L8.55809517,7.43294953 C8.23531459,7.74611298 8.23531459,8.25388736 8.55809517,8.56693769 L12,11.9062921 L9.84187871,14 L4.24208544,8.56693751 C3.91930485,8.25388719 3.91930485,7.74611281 4.24208544,7.43294936 L9.84199531,2 L11.9998836,4.09370803 Z"
+        ></path>
+      </svg>
+      Go back
+    </nuxt-link>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Error',
+  props: ['error']
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,22 +1,38 @@
+import * as SITE_INFO from './assets/site/info.json'
+
 export default {
   /*
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
   target: 'static',
+  env: {
+    url:
+      process.env.NODE_ENV === 'production'
+        ? process.env.URL || 'http://createADotEnvFileAndSetURL'
+        : 'http://localhost:3000',
+    lang: SITE_INFO.sitelang || 'en-US'
+  },
+  generate: {
+    // routes: dynamicRoutes,
+    fallback: true,
+    // subFolders: false,
+    dir: 'public'
+  },
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: SITE_INFO.sitename || process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content:
+          SITE_INFO.sitedescription || process.env.npm_package_description || ''
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
@@ -58,11 +74,11 @@ export default {
    ** See https://content.nuxtjs.org/configuration
    */
   content: {
-    markdown: {
-      prism: {
-        theme: 'prism-themes/themes/prism-material-oceanic.css'
-      }
-    },
+    // markdown: {
+    //   prism: {
+    //     theme: 'prism-themes/themes/prism-material-oceanic.css'
+    //   }
+    // },
     nestedProperties: ['author.name']
   },
   /*
